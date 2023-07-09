22650946394ae8e12715e7898fbc42c2001d479a
Rohini <rohinisreej27092002@gmail.com>
2023-07-09 12:51:10 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner scanner=new Scanner(System.in);
+       int number=scanner.nextInt();
+       while(number!=1)
+       {
+           System.out.print(number+" ");
+           if(number%2==0)
+           {
+               number=number/2;
+           }
+           else
+           {
+               number=(3*number)+1;
+           }
+       }
+        System.out.print(number+" ");
+
+    }
+}
